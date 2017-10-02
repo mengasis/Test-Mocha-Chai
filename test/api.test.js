41dacbe1f72d83e90162bd479679f32afe8044ef
Manuel Venegas <mengasis@outlook.com>
2017-10-02 19:11:14 -0300

    eslint settings

--- test/api.test.js
+++ test/api.test.js
@@ -4,14 +4,14 @@ import { expect } from 'chai'
 const URLBASE = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=CLP'
 
 describe('API Bitcoin', () => {
-    describe('Bitcoin to CLP', () => {
-        it('return status HTTP 200', (done) => {
-            fetch(URLBASE)
+	describe('Bitcoin to CLP', () => {
+		it('return status HTTP 200', (done) => {
+			fetch(URLBASE)
             .then((res) => {
-                expect(res.status).to.equal(200)
-                done()
-            })
-        })
-    })
+	expect(res.status).to.equal(200)
+	done()
+})
+		})
+	})
 })
 
