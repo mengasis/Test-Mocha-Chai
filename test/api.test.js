49ebc0ab16ec80c09440a815202c9a860027eeb9
Manuel Venegas <mengasis@outlook.com>
2017-10-03 12:08:57 -0300

    Test status API Bitcoin and props

--- test/api.test.js
+++ test/api.test.js
@@ -3,15 +3,21 @@ import { expect } from 'chai'
 
 const URLBASE = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=CLP'
 
-describe('API Bitcoin', () => {
-	describe('Bitcoin to CLP', () => {
-		it('return status HTTP 200', (done) => {
-			fetch(URLBASE)
-            .then((res) => {
-	expect(res.status).to.equal(200)
-	done()
-})
-		})
-	})
-})
 
+describe('**API Bitcoin**', () => {
+
+	var response, result
+
+	before(async () => {
+		response = await fetch(URLBASE)
+		result = await response.json()
+		result = result[0]
+	})
+
+	describe('Status API', async () =>{
+		it('Status HTTP 200', () => expect(response.status).to.equal(200))
+		it('Is result a Object?', () => expect(result).to.be.an('object'))
+		it('Has prop price_clp?', () => expect(result).to.have.ownProperty('price_clp'))
+	})
+
+})
